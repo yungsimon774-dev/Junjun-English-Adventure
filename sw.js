25f559184ce35e6041fe9e8cd0aaf8a76118fe39
yungsimon774-dev <yungsimon774@gmail.com>
2026-07-17 16:00:12 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='junjun-english-v1-2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  if(new URL(e.request.url).origin!==location.origin) return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
