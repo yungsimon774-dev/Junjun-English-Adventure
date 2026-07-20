8d9bf286ac994b8cf654fe659cad946e5355074b
yungsimon774-dev <yungsimon774@gmail.com>
2026-07-20 21:35:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE='junjun-english-v1.4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  if(new URL(e.request.url).origin!==location.origin) return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
+const CACHE="junjun-english-v20";
+const ASSETS=["./","./index.html?v=20","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put("./index.html?v=20",c));return r}).catch(()=>caches.match("./index.html?v=20")));return;}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
